ff3f2e9a74ecbca939b86896557a859a680e7a84
YoussefHesham <YoussefHesham@TRUFLAEGY116>
2023-02-12 21:54:55 +0200

    Use Tailwind Grid

--- src/Results.jsx
+++ src/Results.jsx
@@ -2,7 +2,7 @@ import Pet from "./Pet";
 
 const Results = ({ pets }) => {
   return (
-    <section className="search">
+    <section className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 ">
       {!pets.length ? (
         <h1> No Pets Found</h1>
       ) : (
--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -20,8 +20,9 @@ const SearchParams = () => {
   // eslint-disable-next-line no-unused-vars
   const [adoptedPet, _] = useContext(AdoptedPetContext);
   return (
-    <div className="search-params">
+    <div className="my-0 mx-auto w-11/12">
       <form
+        className="mb-10 flex flex-col items-center rounded-lg bg-gray-200 p-10 shadow-lg"
         onSubmit={(e) => {
           e.preventDefault();
           const formData = new FormData(e.target);
@@ -38,18 +39,23 @@ const SearchParams = () => {
             <img src={adoptedPet.images[0]} alt={adoptedPet.name}></img>
           </div>
         )}
-        <div></div>
-        <div></div>
 
         <label htmlFor="location">
           {" "}
           Location
-          <input name="location" id="location" placeholder="Location"></input>
+          <input
+            name="location"
+            id="location"
+            className="search-input"
+            placeholder="Location"
+            type="text"
+          ></input>
         </label>
         <label htmlFor="animal">
           Animal
           <select
             id="animal"
+            className="search-input"
             value={animal}
             placeholder="Animal"
             onChange={(e) => setAnimal(e.target.value)}
@@ -62,6 +68,7 @@ const SearchParams = () => {
         <label htmlFor="breed">
           Breed
           <select
+            className="search-input grayed-out-disabled"
             disabled={breeds.length === 0}
             id="breed"
             placeholder="breed "
@@ -72,7 +79,10 @@ const SearchParams = () => {
             ))}
           </select>
         </label>
-        <button> Submit </button>
+        <button className="color rounded bg-orange-500 px-6 py-2 text-white hover:opacity-50">
+          {" "}
+          Submit{" "}
+        </button>
       </form>
       {<p>Here is some changes</p>}
       <Results pets={pets} />
