6445076b221ced60dd2ee32f86201f4ef3abe1d2
yosefHesham <youssef.h@trufla.com>
2023-10-31 13:21:30 +0200

    Add vite

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
 const Pet = (props) => {
   return React.createElement(
     "div",
@@ -34,5 +37,6 @@ const App = () => {
 };
 
 const container = document.querySelector("#root");
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
+
 root.render(React.createElement(App));
