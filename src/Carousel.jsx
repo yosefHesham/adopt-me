813e88ff05d640cb39c125c9ed18aaf80223a5e9
yosefHesham <youssef.h@trufla.com>
2023-11-01 11:37:42 +0200

    Add Carousel Class

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -9,6 +9,9 @@ class Carousel extends Component {
     images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
   };
 
+  handleIndexClick = (index) => {
+    this.setState({ active: index });
+  };
   render() {
     const { active } = this.state;
     const { images } = this.props;
@@ -20,9 +23,13 @@ class Carousel extends Component {
         <div className="carousel-smaller">
           {images.map((photo, index) => {
             return (
+              //eslint-disable-next-line
               <img
                 key={photo}
                 src={photo}
+                onClick={() => {
+                  this.handleIndexClick(index);
+                }}
                 alt="animal thumbnail"
                 className={index === active && "active"}
               ></img>
